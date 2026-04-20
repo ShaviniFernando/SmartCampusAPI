8574c6af32631cdb350191ab95f53613931acf56
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-04-20 21:26:34 +0530

    feat: add request/response logging filter

---
+++ SmartCampusAPI/src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,28 @@
+package com.smartcampus.filter;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info(String.format("Incoming Request: %s %s", 
+            requestContext.getMethod(), 
+            requestContext.getUriInfo().getPath()));
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info(String.format("Outgoing Response: Status %d", 
+            responseContext.getStatus()));
+    }
+}

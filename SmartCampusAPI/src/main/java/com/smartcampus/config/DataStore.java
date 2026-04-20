0d2b8b6db8392ebaf7c0f1b29954a7dcdfef41fe
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-04-20 21:18:49 +0530

    feat: add in-memory data store for sensors

--- SmartCampusAPI/src/main/java/com/smartcampus/config/DataStore.java
+++ SmartCampusAPI/src/main/java/com/smartcampus/config/DataStore.java
@@ -1,13 +1,19 @@
 package com.smartcampus.config;
 
 import com.smartcampus.model.Room;
+import com.smartcampus.model.Sensor;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DataStore {
     private static final Map<String, Room> rooms = new ConcurrentHashMap<>();
+    private static final Map<String, Sensor> sensors = new ConcurrentHashMap<>();
 
     public static Map<String, Room> getRooms() {
         return rooms;
+    }
+
+    public static Map<String, Sensor> getSensors() {
+        return sensors;
     }
 }

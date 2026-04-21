954f0a05457e17c5ae6610ad377707e39263f78c
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-04-21 10:00:00 +0530

    feat: implement SensorResource GET /sensors/{id}

--- SmartCampusAPI/src/main/java/com/smartcampus/resource/SensorResource.java
+++ SmartCampusAPI/src/main/java/com/smartcampus/resource/SensorResource.java
@@ -2,8 +2,10 @@ package com.smartcampus.resource;
 
 import com.smartcampus.config.DataStore;
 import com.smartcampus.model.Sensor;
+import com.smartcampus.exception.ResourceNotFoundException;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
@@ -19,5 +21,16 @@ public class SensorResource {
             return DataStore.getSensorsByType(type);
         }
         return DataStore.getAllSensors().values();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Sensor getSensorById(@PathParam("id") String id) {
+        Sensor sensor = DataStore.getSensorById(id);
+        if (sensor == null) {
+            throw new ResourceNotFoundException("Sensor with ID " + id + " not found");
+        }
+        return sensor;
     }
 }

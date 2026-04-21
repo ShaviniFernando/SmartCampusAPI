0597cad4acfea7525aa07b7f43d33c3fc4f5e271
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-04-21 13:15:00 +0530

    feat: add ExceptionMapper mapped for InvalidInputException (422)

--- SmartCampusAPI/src/main/java/com/smartcampus/exception/InvalidInputException.java
+++ SmartCampusAPI/src/main/java/com/smartcampus/exception/InvalidInputException.java
@@ -1,12 +1,7 @@
 package com.smartcampus.exception;
 
-import jakarta.ws.rs.WebApplicationException;
-import jakarta.ws.rs.core.Response;
-
-public class InvalidInputException extends WebApplicationException {
+public class InvalidInputException extends RuntimeException {
     public InvalidInputException(String message) {
-        super(Response.status(422) // Unprocessable Entity
-                .entity(new ErrorResponse(message, 422))
-                .build());
+        super(message);
     }
 }

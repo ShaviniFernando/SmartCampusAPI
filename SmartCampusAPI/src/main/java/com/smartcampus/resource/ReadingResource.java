64967930faa1931925cfbb2e8fd8f66f42e36abe
shavinifernando25 <dulsha.20242188@iit.ac.lk>
2026-04-21 12:45:00 +0530

    feat: implement GET and POST methods for sensor readings

--- SmartCampusAPI/src/main/java/com/smartcampus/resource/ReadingResource.java
+++ SmartCampusAPI/src/main/java/com/smartcampus/resource/ReadingResource.java
@@ -1,8 +1,18 @@
 package com.smartcampus.resource;
 
+import com.smartcampus.config.DataStore;
+import com.smartcampus.model.Reading;
+import com.smartcampus.model.Sensor;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.ForbiddenException;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -14,4 +24,27 @@ public class ReadingResource {
         this.sensorId = sensorId;
     }
 
+    @GET
+    public List<Reading> getReadings() {
+        return DataStore.getSensorReadings(sensorId);
+    }
+
+    @POST
+    public Response addReading(Reading reading, @Context UriInfo uriInfo) {
+        Sensor sensor = DataStore.getSensorById(sensorId);
+        
+        if ("MAINTENANCE".equalsIgnoreCase(sensor.getStatus())) {
+            throw new ForbiddenException("Cannot add reading: Sensor " + sensorId + " is in MAINTENANCE mode");
+        }
+        
+        if (reading.getTimestamp() == 0) {
+            reading.setTimestamp(System.currentTimeMillis());
+        }
+        
+        DataStore.addReadingToSensor(sensorId, reading);
+        
+        return Response.status(Response.Status.CREATED)
+                .entity(reading)
+                .build();
+    }
 }
